5806c523bb6a85eecda48e43da9e71517c1f524f
anhad13 <anhadmohananey@gmail.com>
2019-06-20 18:50:47 -0400

    cimment remove

--- utils/data_loader.py
+++ utils/data_loader.py
@@ -57,26 +57,6 @@ def tree2list(tree):
     return []
 
 
-# def build_tree(depth, sen):
-#     assert len(depth) == len(sen)  
-#     if len(depth) == 1:
-#         parse_tree = sen[0]
-#     else:
-#         idx_max = numpy.argmax(depth)
-#         parse_tree = []
-#         if len(sen[:idx_max]) > 0:
-#             tree0 = build_tree(depth[:idx_max], sen[:idx_max])
-#             parse_tree.append(tree0)
-#         tree1 = sen[idx_max]
-#         if len(sen[idx_max + 1:]) > 0:
-#             tree2 = build_tree(depth[idx_max + 1:], sen[idx_max + 1:])
-#             tree1 = [tree1, tree2]
-#         if parse_tree == []:
-#             parse_tree = tree1
-#         else:
-#             parse_tree.append(tree1)
-#     return parse_tree
-
 def build_tree(depth, sen):
     assert len(depth) == len(sen) - 1
     if len(sen)==1 or len(sen) == 2:
