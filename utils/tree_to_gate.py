07835cab2c90dc3d1cf970c21eb9fafe8aab8fa1
anhad13 <anhadmohananey@gmail.com>
2019-06-25 21:45:50 -0400

    cuda

---
+++ utils/tree_to_gate.py
@@ -0,0 +1,25 @@
+#l : length of sentence, tree is list.
+def tree_to_gates(tree, l):
+	arr = [0] * l
+	assign_recursively(tree, arr, 0)
+	return arr
+
+
+def assign_recursively(tree, arr, base = 0, max=10):
+	if type(tree) == list:
+		assign_recursively(tree[0], arr, base, max-1)
+		right_child = tree[1]
+		while(1):
+			if type(right_child) == str:
+				arr[base+leafno(tree[0])] = max
+				break
+			else:
+				right_child = right_child[0]
+		assign_recursively(tree[1], arr, base+leafno(tree[0]), max-1)
+
+
+def leafno(x):
+	if type(x)==str or len(x)==1:
+		return 1
+	else:
+		return leafno(x[0]) + leafno(x[1])
