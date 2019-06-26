6b1534c6d5935eb19a99f2d8b3a5a50ffd96d440
Katharina Kann <katharinakann@10-19-168-58.dynapool.wireless.nyu.edu>
2019-06-26 12:13:23 -0400

    small changes

--- utils/tree_to_gate.py
+++ utils/tree_to_gate.py
@@ -1,9 +1,11 @@
-#l : length of sentence, tree is list.
 def tree_to_gates(tree):
-	l = leafno(tree)
-	arr = [0] * l
-	assign_recursively(tree, arr, 0, max = l)
-	return arr
+    #l : length of sentence, tree is list.
+    if len(tree) == 1:
+        return [1]
+    l = leafno(tree)
+    arr = [1] * l
+    assign_recursively(tree, arr, 0, max = l+1)
+    return arr
 
 
 def assign_recursively(tree, arr, base = 0, max=10):
@@ -25,7 +27,11 @@ def leafno(x):
 	else:
 		return leafno(x[0]) + leafno(x[1])
 
+
 if __name__ == '__main__':
 	#example: tree:  [[['a', 'b'], 'c'], ['d', 'e']],
 	#no of tokens: 5
-	print(tree_to_gates([[['a', 'b'], 'c'], ['d', 'e']]))
+    print(tree_to_gates([[['a', 'b'], 'c'], ['d', 'e']]))
+    print(tree_to_gates([[['a', 'b'], 'c'], 'e']))
+    print(tree_to_gates(['a', 'b']))
+    print(tree_to_gates(['a']))
