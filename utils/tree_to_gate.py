3a227681981334c3fd775c73cc337c3f557eb697
anhad13 <anhadmohananey@gmail.com>
2019-06-26 17:46:52 -0400

    tree to gate update'

--- utils/tree_to_gate.py
+++ utils/tree_to_gate.py
@@ -12,12 +12,16 @@ def assign_recursively(tree, arr, base = 0, max=10):
 	if type(tree) == list:
 		assign_recursively(tree[0], arr, base, max-1)
 		right_child = tree[1]
+		count = 0
 		while(1):
 			if type(right_child) == str:
 				arr[base+leafno(tree[0])] = max
 				break
+			elif count == 1:
+				break
 			else:
 				right_child = right_child[0]
+				count +=1
 		assign_recursively(tree[1], arr, base+leafno(tree[0]), max-1)
 
 
@@ -32,6 +36,7 @@ if __name__ == '__main__':
 	#example: tree:  [[['a', 'b'], 'c'], ['d', 'e']],
 	#no of tokens: 5
     print(tree_to_gates([[['a', 'b'], 'c'], ['d', 'e']]))
+    print(tree_to_gates([['a','b'],[['c','d'],'e']]))
     print(tree_to_gates([[['a', 'b'], 'c'], 'e']))
     print(tree_to_gates(['a', 'b']))
     print(tree_to_gates(['a']))
