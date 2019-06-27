8ba2db65c7260855c6c940e09a03a386ef6adcdf
Katharina Kann <katharinakann@10-19-168-58.dynapool.wireless.nyu.edu>
2019-06-27 15:02:05 -0400

    script update

--- slurms/runner_UNS.s
+++ slurms/runner_UNS.s
@@ -4,4 +4,4 @@
 #SBATCH --mem=40GB
 #SBATCH --gres=gpu:1
 #SBATCH --output=logs/uns1
-python -u main.py --save trained_models/UNS_1 --batch 64 --alpha 0.0 --epochs 70 --PRPN
+python -u main.py --save trained_models/UNS_1 --batch 16 --alpha 0.0 --epochs 70 --PRPN
