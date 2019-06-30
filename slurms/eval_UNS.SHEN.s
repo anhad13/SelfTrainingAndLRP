894213fb320fef4cb68d0628fc9315dfb1e5e16e
kelina <kann@cis.lmu.de>
2019-06-30 11:35:53 -0400

    minor

--- slurms/eval_UNS.SHEN.s
+++ slurms/eval_UNS.SHEN.s
@@ -9,4 +9,4 @@
 
 model_no=$1
 
-python -u main.py --eval_only --load trained_models/UNS.shen.${model_no}.best --batch 1 --PRPN --shen
+python -u main.py --eval_only --load trained_models/UNS.shen.${model_no} --batch 1 --PRPN --shen
