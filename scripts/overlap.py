ce192541e00368322c6c3e93ad824dbeed1de77d
anhad_cimsBox <anhad@nyu.edu>
2019-07-04 16:37:52 -0400

    overlap2

--- scripts/overlap.py
+++ scripts/overlap.py
@@ -12,25 +12,25 @@ def get_stats(outputs):
 	numReports = len(outputs)
 	agree = 0
 	fullagree_f1 = []
-	av_lenth = {2: [], 3: [], 4: [], 5: []}
-	av_f1 = {2: [], 3: [], 4: [], 5: []}
-	partial_agree = {2: 0, 3: 0, 4: 0, 5: 0}
+	av_lenth = {1: [], 2: [], 3: [], 4: [], 5: []}
+	av_f1 = {1: [], 2: [], 3: [], 4: [], 5: []}
+	partial_agree = {1: 0, 2: 0, 3: 0, 4: 0, 5: 0}
 	for j in range(numSent):
 		brackets = []
 		f1s = []
 		for i in range(numReports):
 			brackets.append(sorted(get_brackets(outputs[i][j]['pred_tree'])[0]))
 			f1s.append(outputs[i][j]['f1'])
+		matched_nos = []
 		for i in range(numReports):
 			match_no = 0
-			matched_nos =[]
 			for k in range(numReports):
 				if brackets[k]==brackets[i]:
 					match_no += 1
-			if match_no < 2 or (match_no in matched_nos):
+			if (match_no in matched_nos):
 				continue
 			else:
-				matched_nos.append(matched_no)
+				matched_nos.append(match_no)
 				partial_agree[match_no]+=1
 				av_lenth[match_no].append(len(outputs[0][j]['example']))
 				av_f1[match_no].append(numpy.mean(f1s))
@@ -38,8 +38,8 @@ def get_stats(outputs):
 			print(str(j)+"-evaluated.")
 	for k in av_lenth:
 		print("Matching "+str(k)+" reports: "+str(partial_agree[k]))
-		print("Average F1 of those sentences: " + str(av_f1[k]))
-		print("Av Length of sentences: " + str(av_f1[k]))
+		print("Average F1 of those sentences: " + str(numpy.mean(av_f1[k])))
+		print("Av Length of sentences: " + str(numpy.mean(av_lenth[k])))
 
 # Example call: python scripts
 if __name__ == '__main__':
