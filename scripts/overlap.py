a57f27e9fa80ed5992d619f3cec4aadf14ca7bd1
anhad13 <anhadmohananey@gmail.com>
2019-07-05 13:16:46 -0400

    find sent

--- scripts/overlap.py
+++ scripts/overlap.py
@@ -4,6 +4,27 @@ sys.path.append(".")
 from utils.data_loader import build_tree, get_brackets
 import numpy
 import torch
+from utils import data_loader
+
+def extract_hm():
+	train_data, valid_data, test_data = data_loader_str.main("data/")
+	hm = {}
+	for i in range(len(train_data[0])):
+		hm[" ",join(train_data[4][i])] = train_data[3][i]
+	print("Extracted hashmap to goldtrees.")
+	return hm
+
+def f1compute(pred_brackets, gold_brackets):
+	overlap = pred_brackets.intersection(gold_brackets)    
+    prec = float(len(overlap)) / (len(pred_brackets) + 1e-8)
+    reca = float(len(overlap)) / (len(gold_brackets) + 1e-8)
+    if len(gold_brackets) == 0:
+        reca = 1.
+        if len(pred_brackets) == 0: 
+            prec = 1.
+    f1 = 2 * prec * reca / (prec + reca + 1e-8)
+    return f1
+
 
 def get_stats(outputs):
 	numSent = len(outputs[0])
@@ -16,14 +37,18 @@ def get_stats(outputs):
 	av_f1 = {2: [], 3: [], 4: [], 5: []}
 	av_f1_diff = {2: [], 3: [], 4: [], 5: []}
 	partial_agree = {2: 0, 3: 0, 4: 0, 5: 0}
+	hm = extract_hm()
 	for j in range(numSent):
 		brackets = []
 		f1s = []
+		gold_brackets = hm[" ".join(outputs[0][j]['example'])]
 		for i in range(numReports):
 			brackets.append(sorted(get_brackets(outputs[i][j]['pred_tree'])[0]))
-			f1s.append(outputs[i][j]['f1'])
+			pred_brackets = get_brackets(outputs[i][j]['pred_tree'])[0]
+			computed_f1 = f1compute(pred_brackets, gold_brackets)
+			f1s.append(computed_f1)
 		best_match = 1
-		best_matchf1s=None
+		best_matchf1s=None	
 		for i in range(numReports):
 			match_no = 0
 			matchf1s=[]			
@@ -32,7 +57,7 @@ def get_stats(outputs):
 					match_no += 1
 					matchf1s.append(f1s[k])
 			if best_match < match_no:
-				best_match = match_no
+				best_match = match_no 
 				best_matchf1s = matchf1s
 		if best_match > 1:
 			partial_agree[best_match]+=1
@@ -45,7 +70,7 @@ def get_stats(outputs):
 			av_f1_diff[best_match].append(numpy.mean(best_matchf1s)-numpy.mean(not_match_f1s))
 	for k in av_lenth:
 		print("Matching "+str(k)+" reports: "+str(partial_agree[k]))
-		print("Average F1 of those sentences: " + str(numpy.mean(av_f1[k]))+" .. av diff: "+str(numpy.mean(av_f1_diff[k])))
+		print("Average F1 of those sentences: " + str(numpy.mean(av_f1[k])))
 		print("Av Length of sentences: " + str(numpy.mean(av_lenth[k])))
 		print("Min Length: "+str(numpy.min(av_lenth[k])))
 		print("Max Length: "+str(numpy.max(av_lenth[k])))
