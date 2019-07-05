135155c51899dab71708d602cc1e73cc306a4b85
anhad13 <anhadmohananey@gmail.com>
2019-07-05 16:48:10 -0400

    find sent

--- scripts/overlap.py
+++ scripts/overlap.py
@@ -10,7 +10,7 @@ def extract_hm():
 	train_data, valid_data, test_data = data_loader_str.main("data/")
 	hm = {}
 	for i in range(len(train_data[0])):
-		hm[" ",join(train_data[4][i])] = train_data[3][i]
+		hm[" ".join(train_data[4][i])] = [train[0][i], train[1][i], train[2][i], train[3][i], train[4][i], train[5][i], train_data[6][i]]
 	print("Extracted hashmap to goldtrees.")
 	return hm
 
@@ -21,7 +21,7 @@ def f1compute(pred_brackets, gold_brackets):
     if len(gold_brackets) == 0:
         reca = 1.
         if len(pred_brackets) == 0: 
-            prec = 1.
+        	prec = 1.
     f1 = 2 * prec * reca / (prec + reca + 1e-8)
     return f1
 
@@ -37,11 +37,13 @@ def get_stats(outputs):
 	av_f1 = {2: [], 3: [], 4: [], 5: []}
 	av_f1_diff = {2: [], 3: [], 4: [], 5: []}
 	partial_agree = {2: 0, 3: 0, 4: 0, 5: 0}
+	todump = [[],[],[],[],[],[],[]]
 	hm = extract_hm()
 	for j in range(numSent):
 		brackets = []
 		f1s = []
-		gold_brackets = hm[" ".join(outputs[0][j]['example'])]
+		fulldata = hm[" ".join(outputs[0][j]['example'])]
+		gold_brackets = fulldata[3]
 		for i in range(numReports):
 			brackets.append(sorted(get_brackets(outputs[i][j]['pred_tree'])[0]))
 			pred_brackets = get_brackets(outputs[i][j]['pred_tree'])[0]
@@ -64,10 +66,16 @@ def get_stats(outputs):
 			av_lenth[best_match].append(len(outputs[0][j]['example']))
 			av_f1[best_match].append(numpy.mean(best_matchf1s))
 			not_match_f1s = []
-			for el in f1s:
+			for el in f1s: 
 				if el not in best_matchf1s:
 					not_match_f1s.append(el)
 			av_f1_diff[best_match].append(numpy.mean(best_matchf1s)-numpy.mean(not_match_f1s))
+			if best_match == 5:
+				for i in range(7):
+					todump[i].append(fulldata[i])
+	pickle.dump(open("5match.data", "wb"))
+
+
 	for k in av_lenth:
 		print("Matching "+str(k)+" reports: "+str(partial_agree[k]))
 		print("Average F1 of those sentences: " + str(numpy.mean(av_f1[k])))
